eeb070869f68bb540d0a03a99bc3daa97b5a9895
Ishan <ishan.sandhu3121@gmail.com>
2025-07-28 02:51:59 -0700

    fixed minor issues mentioned by trag and added requirements.txt file

--- server.py
+++ server.py
@@ -1,10 +1,20 @@
 import socket
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# get the port from the environment variable
+port = os.getenv("PORT")
+url = os.getenv("URL")
+
+print(f"Server is running on {url}:{port}")
 
 #  server source address (local host) along with port
-address = ("127.0.0.1", 8080)
+address = (url, int(port))
 
-# creating socket object
-tcp  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# creating socket object (AF_INET (address family): IPv4, SOCK_STREAM (socket type): TCP)
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # binding the socket object to the address
 tcp.bind(address)
